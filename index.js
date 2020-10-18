0b0a73736849886483331c307851d4ff463d1b99
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-10-18 20:24:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,3 @@ function multiply() {
 function divide() {
   return a / b;
 }
-
-var number = 10
-
-function add5() {
-  number += 5
-}
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-console.log(number)
-add5()
-console.log(number)
