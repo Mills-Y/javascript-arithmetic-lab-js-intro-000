74f30b042d57c872c967bb7b110729f8b4a34db0
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-10-18 20:27:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function add() {
+function add(a,b) {
   return a + b;
 }
-function subtract() {
+function subtract(a,b) {
   return a - b;
 }
-function multiply() {
+function multiply(a,b) {
   return a * b;
 }
-function divide() {
+function divide(a,b) {
   return a / b;
 }
-function increment() {
+function increment(n) {
   n += 1;
 }
-function decrement() {
+function decrement(n) {
   n -= 1;
 }
